9a1c7a6da56796ab4963284678a122753620259e
Ian Warren <i.warren@auckland.ac.nz>
2020-08-22 18:54:18 +1200

    Removed superfluous code from __init__.py

--- hello/__init__.py
+++ hello/__init__.py
@@ -1,19 +1,5 @@
 from flask import Flask, render_template
 
-# https://www.webforefront.com/django/createreusablejinjatemplates.html
-# https://flask.palletsprojects.com/en/1.1.x/patterns/templateinheritance/
-# https://en.wikipedia.org/wiki/Jinja_(template_engine)
-# https://codeburst.io/jinja-2-explained-in-5-minutes-88548486834e
-# https://realpython.com/primer-on-jinja-templating/#super-blocks
-
-# https://alankent.me/2013/10/19/html-escaping-for-secure-web-pages/
-# https: // portswigger.net / web - security / csrf / tokens
-# https: // portswigger.net / web - security / csrf
-# https: // en.wikipedia.org / wiki / Cross - site_scripting
-# https: // flask.palletsprojects.com / en / 0.12.x / security /
-# https: // owasp.org / www - community / attacks / xss /
-# https: // jinja.palletsprojects.com / en / 2.11.x / templates /  #
-
 
 def create_app():
     app = Flask(__name__)
